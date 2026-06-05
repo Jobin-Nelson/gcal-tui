1a674d41a1056eaefb07de512add0da681f1c8fb
jobin_nelson <jobinnelson369@gmail.com>
2026-06-05 22:28:54 +0530

    refactor: remove tracing logs

--- src/controller/run.rs
+++ src/controller/run.rs
@@ -2,7 +2,6 @@ use crate::{App, Result, logging::initialize_logging};
 
 pub async fn run() -> Result<()> {
     initialize_logging()?;
-    tracing::info!("HELLO");
 
     let app = App::new().await?;
 
